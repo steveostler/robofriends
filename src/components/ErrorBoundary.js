e068ccea904e63563fd5fbab84e9cc93b1a5dc9f
Stephen Ostler <stephenjostler@gmail.com>
2024-06-25 18:21:39 +0100

    error handling

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+	constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    console.error("Error caught in componentDidCatch:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
